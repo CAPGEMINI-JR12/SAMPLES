69c0656349e171f7022040de3a1c66f2ae283ae5
SarathaNatraj <sarathanatrajan17@gmail.com>
2022-05-24 10:25:34 +0530

    test commit

---
+++ react-router-examples-master/src/Components/Model.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { StyleSheet, css } from "aphrodite/no-important";
+
+const styles = StyleSheet.create({
+	container: {},
+	h3: {
+		fontWeight: "400",
+		fontSize: "24px",
+		margin: "0px",
+	},
+	p: {
+		fontWeight: "300",
+		fontSize: "16px",
+	},
+});
+
+const Model = () => {
+	const { model } = useParams();
+	return (
+		<div className={css(styles.container)}>
+			<h3 className={css(styles.h3)}>{model}</h3>
+			<p className={css(styles.p)}>
+			sample 			</p>
+		</div>
+	);
+};
+
+export default Model;
